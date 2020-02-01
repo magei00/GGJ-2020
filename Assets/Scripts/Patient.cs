b3c57adeee46ccee0307b336b439d06d317b3def
Oliver Jørgensen <oljo@itu.dk>
2020-02-01 11:40:45 +0100

    Made the patient lose health, and badly visualized it

--- Assets/Scripts/Patient.cs
+++ Assets/Scripts/Patient.cs
@@ -6,11 +6,30 @@ public class Patient : MonoBehaviour
 {
     // How much blood to lose per second
     public float BloodLossRate = 5f;
-    private float Blood = 100.0f;
+
+    public GameEvent PatientDead;
+
+    public SpriteRenderer BloodSprite;
+
+    public float Blood = 100f;
+
+    public float MaxBlood = 100f;
+
+    private float _bloodHeight;
+
+    void Awake() {
+        _bloodHeight = BloodSprite.size.y;
+    }
 
     void Update()
     {
         Blood -= BloodLossRate * Time.deltaTime;
+
+        if (Blood <= 0f) {
+            PatientDead.Raise();
+        }
+
+        BloodSprite.size = Vector2.Lerp(new Vector2(BloodSprite.size.x, 0f), new Vector2(BloodSprite.size.x, _bloodHeight), Blood / MaxBlood);
     }
 
     public void Heal(float healAmount)
